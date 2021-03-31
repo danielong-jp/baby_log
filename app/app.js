0b6374ea2d112aabd94ad87a637f28937bc09638
Daniel Ong <crimsonknightd@hotmail.com>
2021-04-01 00:18:19 +0900

    add babySchema

--- app/app.js
+++ app/app.js
@@ -1,4 +1,3 @@
-//jshint esversion:6
 require('dotenv').config()
 const express = require("express");
 const ejs = require("ejs");
@@ -40,13 +39,37 @@ const userSchema = new mongoose.Schema({
     email: String,
     password: String,
     googleId: String,
-    secret: String
+    baby: Number
+});
+
+const babySchema = new mongoose.Schema({
+    baby: Number,
+    name: String,
+    entry: {
+        date: Date,
+        details: {
+            time: {
+                hour: Number,
+                min: Number
+            },
+            feeding: {
+                left: Number,
+                right: Number,
+                bottle: Number
+            },
+            diaper: Number,
+            sleep: Boolean,
+            notes: String
+        }
+    }
 });
 
 userSchema.plugin(passportLocalMongoose);
 userSchema.plugin(findOrCreate);
 
 const User = new mongoose.model("User", userSchema);
+const Baby = new mongoose.model("Baby", babySchema);
+
 passport.use(User.createStrategy());
 passport.serializeUser(function(user, done) {
     done(null, user.id);
@@ -74,107 +97,111 @@ passport.use(new GoogleStrategy({
     }
 ));
 
-// ROUTE path
-
-app.get("/", (req, res) => {
-    res.render("home");
-});
+// HOME path
+app.route("/")
+    .get((req, res) => {
+        res.render("home");
+    });
 
 // AUTH PATH
 
-app.get("/auth/google", passport.authenticate("google", { scope: ["profile"] }));
+app.route("/auth/google")
+    .get(passport.authenticate("google", { scope: ["profile"] }));
 
-app.get("/auth/google/secrets", 
-  passport.authenticate("google", { failureRedirect: "/login" }),
-  function(req, res) {
-    // Successful authentication, redirect home.
-    res.redirect("/secrets");
-  });
+app.route("/auth/google/secrets")
+    .get(
+        passport.authenticate("google", { failureRedirect: "/login" }),
+        function(req, res) {
+            // Successful authentication, redirect home.
+            res.redirect("/secrets");
+        }
+    );
 
 // LOGIN path
 
-app.get("/login", (req, res) => {
-    res.render("login");
-});
+app.route("/login")
+    .get((req, res) => {
+        res.render("login");
+    })
+    .post((req, res) => {
+        const user = new User({
+            username: req.body.username, 
+            password: req.body.password
+        });
 
-app.post("/login", (req, res) => {
-    const user = new User({
-        username: req.body.username, 
-        password: req.body.password
-    });
+        req.login(user, (err) => {
+            if (err) {
+                console.log(err);
 
-    req.login(user, (err) => {
-        if (err) {
-            console.log(err);
-
-        } else {
-            passport.authenticate("local")(req, res, () => {
-                res.redirect("/secrets");
-            });
-        }
-    });
-});
-
-// REGISTER path
-
-app.get("/register", (req, res) => {
-    res.render("register");
-});
-
-app.post("/register", (req, res) => {
-    User.register({username: req.body.username}, req.body.password, (err, user) => {
-        if (err) {
-            console.log(err);
-            res.redirect("/register");
-        } else {
-            passport.authenticate("local")(req, res, () => {
-                res.redirect("/secrets");
-            });
-        }
-    });
-});
-
-// LOGOUT path 
-
-app.get("/logout", (req, res) => {
-    req.logout();
-    res.redirect("/");
-})
-
-// SECRET path
-app.get("/secrets", (req, res) => {
-    if (req.isAuthenticated()) {
-        res.render("secrets");
-    } else {
-        res.redirect("/login");
-    }
-}) 
-
-// SUBMIT path
-app.get("/submit", (req, res) => {
-    if (req.isAuthenticated) {
-        res.render("submit");
-    } else {
-        res.redirect("/login");
-    }
-})
-
-app.post("/submit", (req, res) => {
-    const submittedSecret = req.body.secret;
-    
-    User.findById(req.user.id, (err, foundUser) => {
-        if (err) {
-            console.log(err);
-        } else {
-            if (foundUser) {
-                foundUser.secrent = submittedSecret;
-                foundUser.save(() => {
+            } else {
+                passport.authenticate("local")(req, res, () => {
                     res.redirect("/secrets");
                 });
             }
+        });
+    });
+
+// REGISTER path
+app.route("/register")
+    .get((req, res) => {
+        res.render("register");
+    })
+    .post((req, res) => {
+        User.register({username: req.body.username}, req.body.password, (err, user) => {
+            if (err) {
+                console.log(err);
+                res.redirect("/register");
+            } else {
+                passport.authenticate("local")(req, res, () => {
+                    res.redirect("/secrets");
+                });
+            }
+        });
+    });
+
+// LOGOUT path 
+
+app.route("/logout")
+    .get((req, res) => {
+        req.logout();
+        res.redirect("/");
+    });
+
+// SECRET path
+app.route("/secrets")
+    .get((req, res) => {
+        if (req.isAuthenticated()) {
+            res.render("secrets");
+        } else {
+            res.redirect("/login");
+        }
+    });
+
+// SUBMIT path
+app.route("/submit")
+    .get((req, res) => {
+        if (req.isAuthenticated) {
+            res.render("submit");
+        } else {
+            res.redirect("/login");
         }
     })
-})
+    .post((req, res) => {
+        const submittedSecret = req.body.secret;
+        
+        User.findById(req.user.id, (err, foundUser) => {
+            if (err) {
+                console.log(err);
+            } else {
+                if (foundUser) {
+                    foundUser.secrent = submittedSecret;
+                    foundUser.save(() => {
+                        res.redirect("/secrets");
+                    });
+                }
+            }
+        });
+    });
 
 app.listen(3000, () => {
     console.log("Listening on port 3000");
---
+++ uris.js
@@ -0,0 +1,5 @@
+module.exports = {
+    GOOGLE_CALLBACK_URL: "http://localhost:3000/auth/google/secrets",
+    GOOGLE_USER_PROFILE_URL: "https://www.googleapis.com/oauth2/v3/userinfo",
+    MONGO_USERDB: "mongodb://mongodb:27017/userDB"
+}
